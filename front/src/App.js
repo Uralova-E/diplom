14135351f7b9d0d27f98e8241c577683fd5c6b0a
martynovappolina <polinagalaxytab27@gmail.com>
2023-04-30 22:33:04 +0300

    add routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import Header from './components/Header/Header';
+import MainPage from './pages/MainPage/MainPage';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <MainPage />
+    },
+  ])
+
   return (
+  <>
     <Header />
+    <RouterProvider router={router} />
+  </>
   );
 }
 
---
+++ front/src/pages/MainPage/MainPage.js
@@ -0,0 +1,9 @@
+import './MainPage.scss'
+
+const MainPage = () => {
+    return <div className='main-page'>
+        MainPage
+    </div>
+}
+
+export default MainPage
