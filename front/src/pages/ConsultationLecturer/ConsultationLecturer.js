bef6b8877c6752cc90190a8435a21e6d4810e95a
martynovappolina <polinagalaxytab27@gmail.com>
2023-05-03 22:01:22 +0300

    add prev consultations table (lecturer)

--- front/src/pages/ConsultationLecturer/ConsultationLecturer.js
+++ front/src/pages/ConsultationLecturer/ConsultationLecturer.js
@@ -5,22 +5,53 @@ import { baseURL } from '../../variables'
 import { useEffect, useState } from 'react';
 
 const ConsultationLecturer = () => {
-    const [consultations, setConsultations] = useState([])
+    const [consultationsNext, setConsultationsNext] = useState([])
+    const [consultationsPrev, setConsultationsPrev] = useState([])
+
+    const [consultationsPrevIsVisible, setConsultationsPrevIsVisible] = useState(false)
 
     useEffect(() => {
-        axios.get(`${baseURL}consultation/list/`).then(response => setConsultations(response.data))
-    })
+        axios.get(`${baseURL}consultation/list/`).then(
+            response => {
+                response.data.map((item) => {
+                    if( new Date(item.date).getMilliseconds > new Date().getMilliseconds) {
+                        setConsultationsNext(consultationsNext => [...consultationsNext, item])
+                    } else {
+                        setConsultationsPrev(consultationsPrev => [...consultationsPrev, item])
+                    }
+                })
+    })}, [])
 
     return <div className='container'>
         <div className='title'>
             Список запланированных консультаций
         </div>
-        <ConsultationsTable consultations={consultations} />
+        {
+            consultationsNext.length === 0?
+            <div className='consultation-lecturer-text'>
+                Запланированных консультаций пока нет. Чтобы добавить новую консультацию, нажмите на кнопку "Добавить". 
+            </div>:
+            <ConsultationsTable consultations={consultationsNext} />
+        }
+
         <div  
         className='button'
         onClick={() => {}}> 
         Добавить 
         </div>
+
+        <div  
+        className='button'
+        onClick={() => setConsultationsPrevIsVisible(!consultationsPrevIsVisible)}> 
+        {
+            consultationsPrevIsVisible? 'Скрыть прошедшие консультации': 'Показать прошедшие консультации '
+        }
+        </div>
+
+        {
+            consultationsPrevIsVisible && <ConsultationsTable consultations={consultationsPrev} />
+        }
+
     </div>
 }
 
