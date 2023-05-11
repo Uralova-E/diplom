2b8d172181ae24157057bc83d59504f1eb2bbad2
martynovappolina <polinagalaxytab27@gmail.com>
2023-05-11 18:28:48 +0300

    add buttons to admin page

--- front/src/pages/AdminPanel/AdminPanel.js
+++ front/src/pages/AdminPanel/AdminPanel.js
@@ -13,7 +13,22 @@ const AdminPanel = () => {
 
     return (
         <div className='container'>
-
+            <div className='admin-panel-wrapper'>
+                <div className='admin-panel'>
+                    <div className='admin-panel-button'>Аудитория</div>
+                    <div className='admin-panel-button'>Группа</div>
+                    <div className='admin-panel-button'>Дисциплина</div>
+                    <div className='admin-panel-button'>Должность</div>
+                    <div className='admin-panel-button'>Запись студента</div>
+                    <div className='admin-panel-button'>Кафедра</div>
+                    <div className='admin-panel-button'>Консультация</div>
+                    <div className='admin-panel-button'>Корпус</div>
+                    <div className='admin-panel-button'>Преподаватель</div>
+                    <div className='admin-panel-button'>Студент</div>
+                    <div className='admin-panel-button'>Тип аудитории</div>
+                    <div className='admin-panel-button'>Факультет</div>
+                </div>
+            </div>
         </div>
     )
 } 
